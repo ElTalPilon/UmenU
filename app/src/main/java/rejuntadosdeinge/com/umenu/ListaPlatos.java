01ecb69743d653297f9d3a86647ea23c39f8e3d9
José Pablo Flores G <josepablofg@gmail.com>
2014-09-26 11:47:19 -0600

    Fixed Lista Platos   - Created listView   - Pulls Dummy Data from ArrayList   - Linked each item click to Detalles Plato   - Linked action bar icon to Detalles Soda

--- app/src/main/java/rejuntadosdeinge/com/umenu/ListaPlatos.java
+++ app/src/main/java/rejuntadosdeinge/com/umenu/ListaPlatos.java
@@ -24,28 +24,26 @@ public class ListaPlatos extends ActionBarActivity {
 
         // Dummy data for the ListView.
         String[] platosArray = {
+                "Plato Especial",
                 "Plato Básico A",
                 "Plato Básico B",
-                "Plato Especial",
                 "Plato Vegetariano"
         };
 
         List<String> listaPlatosProvisional = new ArrayList<String>(Arrays.asList(platosArray));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        ArrayAdapter<String> listaPlatosAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.list_item,
                 R.id.list_item_textview,
                 listaPlatosProvisional);
 
-        final ListView listView = (ListView) this.findViewById(R.id.lista_sodas);
-        listView.setAdapter(adapter);
-
-
+        ListView listView = (ListView) this.findViewById(R.id.lista_platos);
+        listView.setAdapter(listaPlatosAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                goToDetallesPlato(listView);
+                goToDetallesPlato(view);
             }
         });
     }
@@ -54,7 +52,7 @@ public class ListaPlatos extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.lista_sodas, menu);
+        getMenuInflater().inflate(R.menu.lista_platos, menu);
         return true;
     }
 
@@ -64,15 +62,28 @@ public class ListaPlatos extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id == R.id.action_info) {
+            goToDetallesSoda((ListView) this.findViewById(R.id.lista_platos));
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
 
     /**
-     * Called when the user clicks the Soda Detail Button
+     * Called when the user clicks any dish item
      */
     public void goToDetallesPlato(View view){
         Intent intent = new Intent(this, DetallesPlato.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user clicks the Soda Detail button
+     * @param view
+     */
+    public void goToDetallesSoda(View view){
+        Intent intent = new Intent(this, DetallesSoda.class);
         startActivity(intent);
     }
 }
