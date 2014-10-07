b2ad5f0c99640818341ed4cbb20ce8b864e25345
dagokiva <dagokiva@gmail.com>
2014-10-07 16:57:15 -0600

    Dummy popup de califiacar plato, si se presiona una estrella en un plato, sale un dialog para comentar y/o modificar la nota q acaba de presionar

--- app/src/main/java/rejuntadosdeinge/com/umenu/DetallesPlato.java
+++ app/src/main/java/rejuntadosdeinge/com/umenu/DetallesPlato.java
@@ -1,25 +1,38 @@
 package rejuntadosdeinge.com.umenu;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RatingBar;
-import android.widget.Toast;
+import android.widget.TextView;
+
 
 
 public class DetallesPlato extends ActionBarActivity {
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.fragment_calificar_plato);
+        dialog.setTitle(getString(R.string.agregueComentario));
+        TextView text = (TextView) dialog.findViewById(R.id.escribaComentario);
+        text.setText(getString(R.string.Comentario));
+
+
         setContentView(R.layout.activity_detalles_plato);
         RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                popUpPuntuarPlato();
+                float nota= ratingBar.getRating();
+                popUpPuntuarPlato(dialog, nota);
             }
         });
     }
@@ -37,8 +50,12 @@ public class DetallesPlato extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void popUpPuntuarPlato() {
-        Toast msg = Toast.makeText(this, "Puntuar Plato", Toast.LENGTH_LONG);
-        msg.show();
+    private void popUpPuntuarPlato(Dialog dialog, float nota) {
+        RatingBar rb = (RatingBar) dialog.findViewById(R.id.ratingBarPopUp);
+        rb.setRating(nota);
+        dialog.show();
+
+        //Toast msg = Toast.makeText(this, "Puntuar Plato: "+ nota, Toast.LENGTH_LONG);
+        //msg.show();
     }
 }
