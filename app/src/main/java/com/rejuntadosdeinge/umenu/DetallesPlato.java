f61f9775342cffbdc80bcae9c15ab4673b73cd5a
dagokiva <dagokiva@gmail.com>
2014-11-05 11:27:54 -0600

    Un comentario para q no se ve vaya a una página web y aclararición q hay datos quemados

--- app/src/main/java/com/rejuntadosdeinge/umenu/DetallesPlato.java
+++ app/src/main/java/com/rejuntadosdeinge/umenu/DetallesPlato.java
@@ -117,6 +117,7 @@ public class DetallesPlato extends ActionBarActivity {
                 RatingBar rb1 = (RatingBar) dialog.findViewById(id.ratingBarPopUp);
                 //float notaFinal=rb1.getRating();
 
+                //datos quemados
                 int id = 94;
                 int calif = 5;
                 int total = 25;
@@ -132,7 +133,7 @@ public class DetallesPlato extends ActionBarActivity {
                 uri.append(promedio);
 
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString()));
-                startActivity(intent);
+                //startActivity(intent);
 
 
             }
