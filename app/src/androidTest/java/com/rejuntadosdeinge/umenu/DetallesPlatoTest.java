319300281b130525cd3c42525336c72ab3c21805
José Pablo Flores G <josepablofg@gmail.com>
2014-11-07 23:03:17 -0600

    Tests listos con Shared Preferences. Globals borrado.

--- app/src/androidTest/java/com/rejuntadosdeinge/umenu/DetallesPlatoTest.java
+++ app/src/androidTest/java/com/rejuntadosdeinge/umenu/DetallesPlatoTest.java
@@ -1,12 +1,11 @@
 package com.rejuntadosdeinge.umenu;
 
+import android.content.SharedPreferences;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
 
-public class DetallesPlatoTest
-        extends ActivityInstrumentationTestCase2<DetallesPlato> {
-
-    Globals g = Globals.getInstance();
+public class DetallesPlatoTest extends ActivityInstrumentationTestCase2<DetallesPlato> {
+    private SharedPreferences pref;
     private DetallesPlato activity;
     TextView nombreDelPlato;
     TextView precioDelPlato;
@@ -15,13 +14,15 @@ public class DetallesPlatoTest
 
     @Override
     protected void setUp() throws Exception {
-
         super.setUp();
+        SharedPreferences.Editor editor = pref.edit();
+        editor.apply();
 
         //Suponemos que se escogio la soda de derecho
-        g.setIdSoda(2);
-        g.setCategoria("B%C3%A1sico%201");
-        g.setNombreSoda("Derecho");
+        editor.putInt("IDSoda", 2);
+        editor.putString("categoriaPlato", "B%C3%A1sico%201");
+        editor.putString("nombreSoda", "Derecho");
+        editor.commit();
 
         activity = (DetallesPlato) getActivity();
         nombreDelPlato = (TextView) activity.findViewById(R.id.nombre_plato);
